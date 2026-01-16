a39908fe539d55408d4f92daaafab8febebb2d78
Shubham_Jamdar <149092799+Shubham45264@users.noreply.github.com>
2026-01-16 17:00:44 +0530

    Update config.ts

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -1,15 +1,14 @@
-import env from "@/app/env";
-
 import { Client, Account, Avatars, Databases, Storage } from "appwrite";
 
-const client = new Client()
-    .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
-    .setProject(env.appwrite.projectId); // Your project ID
+const client = new Client();
 
-const databases = new Databases(client)
-const account = new Account(client);
-const avatars = new Avatars(client);
-const storage = new Storage(client);
+client
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const avatars = new Avatars(client);
+export const storage = new Storage(client);
 
-export { client, databases, account, avatars, storage}
+export default client;
